c255e7e863ee57488efee127671e587fefa38176
Atanas Kisyov <kisyov92@gmail.com>
2024-09-08 11:01:36 +0200

    Added cors allowing acces to client

--- app.js
+++ app.js
@@ -5,11 +5,19 @@ require("dotenv").config();
 // ℹ️ Connects to the database
 require("./db");
 
+const cors = require("cors");
+
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
 
 const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
